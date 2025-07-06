1292b39745e62e3a7efdeb860ae3c1314b3348fb
Simon Colonia Amador <82274800+Lecho67@users.noreply.github.com>
2025-07-06 14:29:16 -0500

    Fase 0: Configuración inicial del repositorio y estructura de directorios

--- backend/ecommerce_backend/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include # Importa las funciones necesarias para manejar las URLs
-
-urlpatterns = [
-    path('admin/', admin.site.urls), # URL del panel de administración de Django
-    path('api/', include('products.urls')), # Incluye las URLs de la app 'products' bajo el prefijo '/api/'
-    path('api/', include('orders.urls')),   # Incluye las URLs de la app 'orders' bajo el prefijo '/api/'
-]
